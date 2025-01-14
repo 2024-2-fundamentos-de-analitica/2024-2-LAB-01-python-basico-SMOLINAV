dc2b53a2922f86373afd677e7c0f7a5c8e400b5b
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-01-14 10:12:26 -0500

    listo la 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_01():
     """
@@ -14,3 +15,20 @@ def pregunta_01():
     214
 
     """
+
+
+    with open("files/input/data.csv", 'r') as file:
+        lector_csv = csv.reader(file, delimiter='	')  # Usar espacio como delimitador
+
+        suma = 0
+
+        for fila in lector_csv:
+            suma += int(fila[1])
+
+        # Imprimir la suma
+        print(suma)
+
+    return suma
+
+
+pregunta_01()
