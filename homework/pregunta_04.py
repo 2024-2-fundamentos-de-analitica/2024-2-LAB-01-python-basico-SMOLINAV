db6c9973e6db018fe0c2958c75695c39d9e5cde4
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-01-14 10:43:09 -0500

    pregunta 4 lista

--- homework/pregunta_04.py
+++ homework/pregunta_04.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_04():
     """
@@ -26,3 +27,25 @@ def pregunta_04():
      ('12', 3)]
 
     """
+
+    with open("files/input/data.csv", 'r') as file:
+        lector_csv = csv.reader(file, delimiter='	')  # Usar espacio como delimitador
+
+        # Contar cantidad de cada mes
+        contar = {}  # Diccionario para contar la cantidad de cada mes
+
+        for row in lector_csv:  # Iterar sobre las filas del archivo CSV
+            date = row[2]  # Obtener la tercera columna (fecha) de la fila
+            month = date.split('-')[1]  # Obtener el mes de la fecha
+            contar[month] = contar.get(month, 0) + 1  # Incrementar el contador del mes en 1
+
+        # Convertir el diccionario en una lista de tuplas
+        resultado = list(contar.items())
+
+        # Ordenar la lista de tuplas por la primera columna (mes)
+        resultado.sort(key=lambda x: x[0])
+        print(resultado)
+    return resultado
+
+
+pregunta_04()
