90b627045e2b63f641db2920588a74597f97dc4e
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-01-27 09:41:51 -0500

    lista pregunta 9

--- homework/pregunta_09.py
+++ homework/pregunta_09.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_09():
     """
@@ -24,3 +25,34 @@ def pregunta_09():
      'jjj': 18}}
 
     """
+
+    with open("files/input/data.csv", 'r') as file:
+    # Leer el archivo CSV separado por espacios
+        reader = csv.reader(file, delimiter='	')
+
+        # Inicializar un diccionario para contar las apariciones de cada clave
+        counts = {}
+
+        # Iterar sobre las filas del archivo CSV
+        for row in reader:
+            # Obtener la clave de la columna 5
+            dictionary = row[4]
+
+            # Separar el diccionario en pares clave-valor
+            pairs = dictionary.split(',')
+
+            # Iterar sobre los pares clave-valor
+            for pair in pairs:
+                # Obtener la clave y el valor
+                key, value = pair.split(':')
+
+                # Incrementar la cantidad de apariciones de la clave en el diccionario
+                counts[key] = counts.get(key, 0) + 1
+
+        # Ordenar el diccionario alfabeticamente
+        counts = dict(sorted(counts.items()))
+    
+    print(counts)
+    return counts
+
+pregunta_09()
