e9211e45bde23810791e11c71db51c11b5f6addb
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-01-27 09:44:19 -0500

    lista pregunta 11

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_11():
     """
@@ -16,3 +17,31 @@ def pregunta_11():
 
 
     """
+
+    with open("files/input/data.csv", 'r') as file:
+    # Leer el archivo CSV separado por espacios
+        reader = csv.reader(file, delimiter='	')
+
+        # Inicializar un diccionario para contar la suma por cada letra
+        sums = {}
+
+        # Iterar sobre las filas del archivo CSV
+        for row in reader:
+            # Obtener la letra de la columna 4
+            letter = row[3]
+
+            letters = letter.split(',')
+
+            # Obtener el valor de la columna 2
+            value = int(row[1])
+
+            # Sumar el valor al diccionario utilizando la letra como clave
+            for l in letters:
+                sums[l] = sums.get(l, 0) + value
+
+        # Ordenar el diccionario por las claves
+        sums = dict(sorted(sums.items()))
+        print(sums)
+        return sums
+    
+pregunta_11()
