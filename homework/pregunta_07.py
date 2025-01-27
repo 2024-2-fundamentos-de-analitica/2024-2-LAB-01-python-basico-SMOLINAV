7c50830dae0b86ec490deee0beeee8641d63f18d
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-01-27 09:38:33 -0500

    lista pregunta 7

--- homework/pregunta_07.py
+++ homework/pregunta_07.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_07():
     """
@@ -25,3 +26,35 @@ def pregunta_07():
      (9, ['A', 'B', 'E', 'A', 'A', 'C'])]
 
     """
+
+    with open("files/input/data.csv", 'r') as file:
+    # Leer el archivo CSV separado por espacios
+        reader = csv.reader(file, delimiter='	')
+
+        # Inicializar un diccionario para registrar las letras por cada valor de la segunda columna
+        letras_por_valor = {}
+
+        # Iterar sobre las filas del archivo CSV
+        for row in reader:
+            # Obtener el valor de la segunda columna
+            valor = row[1]
+
+            # Obtener la primera columna (letra) de la fila
+            letra = row[0]
+
+            # Agregar la letra a la lista correspondiente al valor en el diccionario
+            if valor in letras_por_valor:
+                letras_por_valor[valor].append(letra)
+            else:
+                letras_por_valor[valor] = [letra]
+
+        # Convertir el diccionario en una lista de tuplas
+        result = [(int(valor), letras) for valor, letras in letras_por_valor.items()]
+
+        # Ordenar la lista de tuplas por la primera columna (valor)
+        result.sort(key=lambda x: x[0])
+        print(result)
+        return result
+    
+
+pregunta_07()
