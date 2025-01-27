8b05268d0eb8c17a76c56d3eb99ea8cef59aaef4
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-01-27 09:43:05 -0500

    lista pregunta 10

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_10():
     """
@@ -20,3 +21,27 @@ def pregunta_10():
 
 
     """
+
+    with open("files/input/data.csv", 'r') as file:
+    # Leer el archivo CSV separado por espacios
+        reader = csv.reader(file, delimiter='	')
+
+        # Inicializar una lista para almacenar las tuplas
+        result = []
+
+        # Iterar sobre las filas del archivo CSV
+        for row in reader:
+            # Obtener la letra de la primera columna
+            letter = row[0]
+
+            # Obtener la cantidad de elementos de las columnas 4 y 5
+            count_4 = len(row[3].split(','))
+            count_5 = len(row[4].split(','))
+
+            # Agregar la tupla al resultado
+            result.append((letter, count_4, count_5))
+
+        print(result)
+        return result
+    
+pregunta_10()
